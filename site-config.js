79d4f57a7f7dc7a99c747b909e41e0ca5e64f5b6
iTheRoken <rokmatpriv@gmail.com>
2026-07-21 15:28:19 -0300

    Fix installer name and release URL formatting

--- site-config.js
+++ site-config.js
@@ -9,7 +9,7 @@
  * releaseUrl. Si se deja vacío, se usa el archivo de la carpeta downloads.
  */
 window.LUNERA_SITE_CONFIG = {
-  installerName: 'Lunera Beta Setup.exe',
+  installerName: 'Lunera_Beta_Setup.exe',
   localInstallerPath: 'downloads/Lunera%20Beta%20Setup.exe',
-  releaseUrl: 'https://github.com/iTheRoken/Lunera.github.io/releases/download/App/Lunera.Beta.Setup.exe'
+  releaseUrl: 'https://github.com/iTheRoken/Lunera.github.io/releases/download/App/Lunera_Beta_Setup.exe'
 };
