efbde87a2c267aaf466ee8afea8d0084ba2edf67
iTheRoken <rokmatpriv@gmail.com>
2026-07-21 00:37:54 -0300

    Update site-config.js

--- site-config.js
+++ site-config.js
@@ -11,5 +11,5 @@
 window.LUNERA_SITE_CONFIG = {
   installerName: 'Lunera Beta Setup.exe',
   localInstallerPath: 'downloads/Lunera%20Beta%20Setup.exe',
-  releaseUrl: 'https://github.com/iTheRoken/Lunera.github.io/releases/download/App/Lunera.Beta.Setup.exe'
+  releaseUrl: 'https://github.com/iTheRoken/Lunera.github.io/releases/download/App/Lunera%20Beta%20Setup.exe'
 };
